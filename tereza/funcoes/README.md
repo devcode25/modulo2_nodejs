83dbb6791b39e070eb7b9b81d8541280593fb1b1
devcode25 <tereza.dev@outlook.com>
2023-05-16 09:41:10 -0300

    Adicionei a pasta do exercício de funções

---
+++ tereza/funcoes/README.md
@@ -0,0 +1,4 @@
+# Função
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/terezadev0/pen/rNqJEKK](https://codepen.io/terezadev0/pen/rNqJEKK).
+
---
+++ tereza/funcoes/dist/script.js
@@ -0,0 +1,28 @@
+function dobro(num) {
+  return num * 2
+}
+console.log(dobro(6))
+
+
+/*function falar() {
+  return 'Pipipi popopo'
+}
+console.log(falar())*/
+
+
+/*function calcularMedia(nota1, nota2, nota3) {
+  const soma = (nota1 + nota2 + nota3)
+  const media = soma / 3
+  return media
+}*/
+
+
+/*const mediaMat = (prova, atividades) => {
+  if (prova > 10 || atividades > 10) {
+    return `somente notas de 0 a 10`
+  } else {
+    return (prova + atividades)/2
+  }
+}
+
+console.log(mediaMat(10, 4));*/
