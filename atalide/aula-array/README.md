6d137094a65f5625f06ab9925b980db524229bb3
Atalide <atallyde02@gmail.com>
2023-05-16 09:41:25 -0300

    Adicionei a pasta do exercicio aula-array

---
+++ atalide/aula-array/README.md
@@ -0,0 +1,4 @@
+# Aula Array
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Atalide/pen/KKGyaqB](https://codepen.io/Atalide/pen/KKGyaqB).
+
---
+++ atalide/aula-array/dist/script.js
@@ -0,0 +1,28 @@
+//criando array
+const frutas = ["maça", "banana","uva","manga"]
+
+console.log(frutas.length)
+
+// Acessando itens
+
+frutas.forEach(function(item,indice, array){
+  console.log(item, indice, array)
+})
+
+//Acessar item
+
+const primeiro = frutas[frutas]
+
+const ultimo = frutas [frutas.length-1]
+
+console.log(primeiro)
+
+console.log(ultimo)
+
+//inserindo elementos
+
+const adicionar = frutas.push("coco")
+
+console.log(frutas)
+
+frutas.splice(1, 0,"banana")
